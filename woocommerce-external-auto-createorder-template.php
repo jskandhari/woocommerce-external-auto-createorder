66f3dfa314e17bb2aca11ac5b05089a18f036d13
Jskandhari <jskandhari@users.noreply.github.com>
2015-07-18 18:46:41 +0530

    Create woocommerce-external-auto-createorder-template.php

---
+++ woocommerce-external-auto-createorder-template.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * External product add to cart
+ *
+ * @author 		Iskandariya Solutions
+ * @package 	woocommerce-external-auto-createorder/woocommerce-external-auto-createorder-template.php
+ * @version     1.0.0
+ 
+ Extended Version of * External product add to cart
+ *
+ * @author 		WooThemes
+ * @package 	WooCommerce/Templates
+ * @version     2.1.0
+ 
+ */
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
